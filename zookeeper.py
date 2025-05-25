d82ee2b93be301d663380e7181c6dfadcdaea1d7
GioMai24 <giovanniandrea.maida@studenti.unipd.it>
2025-05-25 23:53:16 +0200

    More nets

--- zookeeper.py
+++ zookeeper.py
@@ -7,7 +7,7 @@ def convool_size(input_size, kernel_size, stride, padding=0, dilation=1):
     '''
     Returns Conv2d and Max_pool output size for a square input tensor.
     '''
-    if str(padding) == 'same': output_size = input_size
+    if padding == 'same': output_size = input_size
     else: output_size = ((input_size + 2 * padding - dilation * (kernel_size - 1) - 1) // stride) + 1
     return output_size
 
